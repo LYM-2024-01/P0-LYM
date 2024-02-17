3cc057106f68ec5bdb7c71a5da4b734eefd6f80e
JuanDDY <j.duartey@uniandes.edu.co>
2024-02-17 00:14:11 -0500

    lexer fin

--- proyecto0/lexer.py
+++ proyecto0/lexer.py
@@ -8,15 +8,13 @@ separadores = ["", " ", "\n", "\t"]
 
 NUMEROS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-CONSTANTES = ["Dim", "myXpos", "myYpos", "myChips", "myBalloons", "balloonsHere", "ChipsHere", "Spaces"]
+CONSTANTES = ["dim", "myxpos", "myypos", "mychips", "myballoons", "balloonshere", "chipsHere", "spaces"]
 
 O = [":north", ":south", ":east", ":west"]
 
-D = [":left", ":right", ":around"]
+D = [":left", ":right", ":around", ":front", ":back", ":up", ":down"]
 
 X = [":balloons", ":chips"]
-
-DS = [":front", ":right", ":left", ":back"]
 
 NULL = ["null"]
 
@@ -26,7 +24,7 @@ SUBCOMANDOS = ["defvar", "move", "skip", "turn", "face", "put", "pick", "move-di
 
 SUBESTRUCTURA = ['if', 'loop', 'repeat', 'defun']
 
-SUBCONDICIONES = ['facing?', 'blocked?','can-put?', 'can-pick?', 'can-move?', 'isZero?', 'not']
+SUBCONDICIONES = ['facing?', 'blocked?','can-put?', 'can-pick?', 'can-move?', 'iszero?', 'not']
 
 
 
@@ -40,7 +38,6 @@ def es_numero (palabra):
                 flag = False
     else:
         flag = False
-    print("Mirar palabra: " + palabra + " cond- " + str(flag))
     return flag
 
 def es_condicion (palabra):
@@ -52,6 +49,12 @@ def es_condicion (palabra):
 def es_comando (palabra):
     flag = True
     if (palabra not in SUBCOMANDOS):
+        flag = False
+    return flag
+
+def es_estructura (palabra):
+    flag = True
+    if (palabra not in SUBESTRUCTURA):
         flag = False
     return flag
 
@@ -67,11 +70,6 @@ def es_giro (palabra):
         flag = False
     return flag
 
-def es_direccion (palabra):
-    flag = True
-    if (palabra not in DS):
-        flag = False
-    return flag
 
 def es_item (palabra):
     flag = True
@@ -81,60 +79,100 @@ def es_item (palabra):
 
 def es_null (palabra):
     flag = True
-    if (palabra not in SUBESTRUCTURA):
+    if (palabra not in NULL):
+        flag = False
+    return flag
+
+def es_constante (palabra):
+    flag = True
+    if (palabra not in CONSTANTES):
         flag = False
     return flag
 
 
+
+def dar_tipo(palabra):
+    id = ""
+    valor = ""
+
+    if (palabra == "("):
+        id = "LP"
+        valor="(" 
+
+    elif (palabra == ")"):
+        id = "RP"
+        valor=")" 
+    
+    elif (es_numero(palabra)):
+        id = "NUMBER"
+        valor = palabra
+
+    elif (es_condicion(palabra)):
+        id = palabra.upper()
+        valor = palabra
+
+    elif (es_comando(palabra)):
+        id = palabra.upper()
+        valor = palabra
+
+    elif (es_estructura(palabra)):
+        id = palabra.upper()
+        valor = palabra
+
+    elif (es_cardinal(palabra)):
+        id = "O"
+        valor = palabra
+
+    elif (es_giro(palabra)):
+        id = "D"
+        valor = palabra
+
+    elif (es_item(palabra)):
+        id = "X"
+        valor = palabra
+
+    elif (es_null(palabra)):
+        id = "NULL"
+        valor = palabra
+ 
+    elif (es_constante(palabra)):
+        id = "CONSTANTE"
+        valor = palabra
+
+    else: 
+        if (palabra != ""):
+            id = "NAME"
+            valor = palabra
+
+    return id, valor
+
+
+
 def siguiente_palabra (archivo, i):
     
-    adicion = tk.Token("", "")
     palabra = ""
     flag1 = True
     while(flag1):
 
         if (archivo[i] in caracteres_importantes):
             palabra = archivo[i]
-            if (archivo[i] == "("):
-                adicion = tk.Token("LP","(")
-            elif (archivo[i] == ")"):
-                adicion = tk.Token("RP",")")
             flag1 = False
-
 
         elif (archivo[i] not in separadores) :
             palabra += archivo[i]
-            #adicion = tk.Token(palabra,palabra)
-            #print(adicion.valor)
             if(archivo[i+1] in caracteres_importantes):
-                adicion = tk.Token(palabra,palabra)
-                #print(" -- " + adicion.valor)
                 flag1 = False
 
-
         else:
-            if (es_numero(palabra)):
-                adicion = tk.Token("NUMBER",palabra)
-                print("Number "+palabra)
-
-            elif (es_condicion(palabra)):
-                adicion = tk.Token("CONDICION",palabra)
-
-            elif (es_comando(palabra)):
-                adicion = tk.Token("COMANDO",palabra)
-
-            elif (es_cardinal(palabra)):
-                adicion = tk.Token("DIRECCION",palabra)
-
-            else: 
-                if (palabra != ""):
-                    adicion = tk.Token(palabra,palabra)
-            print(" ** " + adicion.valor)
             flag1 = False
-        i+=1
-    #print(adicion.id)
 
-    return adicion.id, i
+        i+=1
+
+    
+    idp, valorp = dar_tipo(palabra)
+
+    adicion = tk.Token(idp, valorp)
+    return adicion, i               # return idp, i   #Cambiar el return de adicion por el de idp y el comentario de mas abajo para imprimir
 
 
 
@@ -158,9 +196,8 @@ def separar_texto (archivo):
 
 
 
-#ruta_archivo=input("Ingrese la ruta del archivo ( por ejemplo: proyecto0/documentos/ejemplo2.txt): ")
 archivo= open("proyecto0/documentos/ejemplo.txt").read().lower()
 
 lista = separar_texto(archivo)
-print(lista)
+#print(lista)            #Quitar comentario para imprimir lista
 
--- proyecto0/token.py
+++ proyecto0/token.py
@@ -3,9 +3,3 @@ class Token:
   def __init__(self, id, valor):
     self.id = id
     self.valor = valor
-
-p1 = Token("lp", "(")
-
-print(p1)
-print(p1.id)
-print(p1.valor)
